46322a729c6a40740aa89282611199672068db21
patryksz18 <patrykkarol07@gmail.com>
2024-10-31 18:50:28 +0100

    zaprojektowanie richboxa i dodanie progressbar

--- WpfApp4/MainWindow.xaml.cs
+++ WpfApp4/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp4
+namespace TextFormatter
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
